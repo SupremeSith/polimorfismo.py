3985089900bef17a3bedffa46595fce7352ea4e5
João Pedro Portocarrero <134610588+SupremeSith@users.noreply.github.com>
2023-10-05 07:34:09 -0300

    Update and rename algoritmo.py to exemplo1.py

---
+++ exemplo1.py
@@ -0,0 +1,5 @@
+#exemplo usando len(), função usada para retornar o número de caracteres na string
+
+lista = ["João Pedro" , "5/10/2006" , "Campinas"]
+print (len(lista))
+#aqui o len é usado para retornar a quantidade de informações na minha lista (meu nome, o dia e o local do meu nascimento) 
