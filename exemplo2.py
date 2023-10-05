41def4446aa2fb41db3267ec57e20f63a93fbb1c
João Pedro Portocarrero <134610588+SupremeSith@users.noreply.github.com>
2023-10-05 07:39:22 -0300

    Create exemplo2.py

---
+++ exemplo2.py
@@ -0,0 +1,3 @@
+#definindo X como um trecho da musica "Descobridor dos sete mares", diferente de uma lista, o len ira conta as caractéres q definem o X
+x = "Pois bem, cheguei Quero ficar bem à vontade Na verdade, eu sou assim Descobridor dos sete mares Navegar eu quero Uma lua me ilumina Com a clareza e o brilho do cristal Transando as cores desta vida Vou colorindo a alegria de chegar Boa Viagem, Ubatuba Grumaí, Leme e Guarujá Praia Vermelha, Ilhabela Braços abertos sempre a esperar"
+print(len(x))
